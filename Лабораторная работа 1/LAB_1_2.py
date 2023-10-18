cad46c2689600837c5cf1f8ee411a6872095fa5e
zn7cococo <148007278+zn7cococo@users.noreply.github.com>
2023-10-18 23:26:03 +0300

    Update LAB_1_2.py

--- Лабораторная работа 1/LAB_1_2.py
+++ Лабораторная работа 1/LAB_1_2.py
@@ -1,2 +1,4 @@
-books = 1.44*1024*1024/(100*50*25*4)
-print("Количество книг, помещающихся на дискету:", int(books))
+discet_byte = 1.44*1024*1024
+books_byte = 100*50*25*4
+books_num = discet_byte/books_byte
+print("Количество книг, помещающихся на дискету:", int(books_num))
